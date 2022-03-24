14af732b400925cbfbe4e1fb1e4086f498ddb8dc
sunyfg <yanfeng_sun@yeah.net>
2022-03-24 13:49:45 +0800

    update

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import Banner from '../../components/Banner'
+import Courses from '../../components/Courses'
+
+const Home = () => {
+  return (
+    <div>
+      <Banner />
+      <Courses />
+    </div>
+  )
+}
+
+export default Home
